50eabb5ec82f858389eb388f8a4502b4c4d46849
vikram_thakre <vthakre5@in.ibm.com>
2019-03-19 15:57:46 +0530

    Employee added

---
+++ demo/src/main/java/com/example/domain/Employee.java
@@ -0,0 +1,35 @@
+package com.example.domain;
+
+public class Employee {
+	
+	String firstName;
+	
+	String lastName;
+	
+	String dob;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+}
