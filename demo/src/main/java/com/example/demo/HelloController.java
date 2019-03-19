e3d25f23b93023b0f0008e1dfbd0735c174e4bcc
vikram_thakre <vthakre5@in.ibm.com>
2019-03-19 15:57:12 +0530

    Changes done to add more rest api

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.domain.Employee;
 
 @RestController
 public class HelloController {
@@ -11,4 +15,9 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
 
+    @RequestMapping("/createEmp")
+    public ResponseEntity<Employee> createEmp(){
+    	
+    	return new ResponseEntity<Employee>(HttpStatus.OK);
+    }
 }
